75625f638ad1b2907815e7e0ea688fcd332a3dd7
KTujjar <Khaled@Tujjar.com>
2026-04-14 22:35:08 -0400

    trusted HID fallback

--- tracing/hid_provenance_monitor.sh
+++ tracing/hid_provenance_monitor.sh
@@ -11,6 +11,32 @@ contains_pair() {
   [[ ",${haystack}," == *",${needle},"* ]]
 }
 
+normalize_hex4() {
+  local value="${1,,}"
+  value="${value//[^0-9a-f]/}"
+  if [[ -z "$value" ]]; then
+    return 1
+  fi
+  while (( ${#value} < 4 )); do
+    value="0${value}"
+  done
+  printf '%s\n' "${value: -4}"
+}
+
+extract_pair_from_product() {
+  local product="$1"
+  local _bus raw_vendor raw_product _ver
+  IFS='/' read -r _bus raw_vendor raw_product _ver <<< "$product"
+  if [[ -z "${raw_vendor:-}" || -z "${raw_product:-}" ]]; then
+    return 1
+  fi
+
+  local vendor product_id
+  vendor="$(normalize_hex4 "$raw_vendor")" || return 1
+  product_id="$(normalize_hex4 "$raw_product")" || return 1
+  printf '%s:%s\n' "$vendor" "$product_id"
+}
+
 emit_event() {
   local action="$1"
   local subsystem="$2"
@@ -43,8 +69,18 @@ flush_block() {
   local devpath="${KV_DEVPATH:-}"
   local vendor_id="${KV_ID_VENDOR_ID:-}"
   local product_id="${KV_ID_MODEL_ID:-}"
+  local product_prop="${KV_PRODUCT:-}"
   local serial="${KV_ID_SERIAL_SHORT:-}"
   local keyboard=false
+
+  # Some virtual HID devices expose vendor/product only via PRODUCT=bus/vendor/product/ver.
+  if [[ ( -z "$vendor_id" || -z "$product_id" ) && -n "$product_prop" ]]; then
+    local pair
+    if pair="$(extract_pair_from_product "$product_prop")"; then
+      vendor_id="${pair%%:*}"
+      product_id="${pair##*:}"
+    fi
+  fi
 
   if [[ "${KV_ID_INPUT_KEYBOARD:-0}" == "1" ]]; then
     keyboard=true
@@ -54,7 +90,7 @@ flush_block() {
     emit_event "$action" "$subsystem" "$devnode" "$devpath" "$vendor_id" "$product_id" "$serial" "$keyboard"
   fi
 
-  unset KV_ACTION KV_SUBSYSTEM KV_DEVNAME KV_DEVPATH KV_ID_VENDOR_ID KV_ID_MODEL_ID KV_ID_SERIAL_SHORT KV_ID_INPUT_KEYBOARD
+  unset KV_ACTION KV_SUBSYSTEM KV_DEVNAME KV_DEVPATH KV_ID_VENDOR_ID KV_ID_MODEL_ID KV_PRODUCT KV_ID_SERIAL_SHORT KV_ID_INPUT_KEYBOARD
 }
 
 udevadm monitor --udev --property --subsystem-match=input |
@@ -81,6 +117,7 @@ while IFS= read -r line; do
     DEVPATH) KV_DEVPATH="$value" ;;
     ID_VENDOR_ID) KV_ID_VENDOR_ID="$value" ;;
     ID_MODEL_ID) KV_ID_MODEL_ID="$value" ;;
+    PRODUCT) KV_PRODUCT="$value" ;;
     ID_SERIAL_SHORT) KV_ID_SERIAL_SHORT="$value" ;;
     ID_INPUT_KEYBOARD) KV_ID_INPUT_KEYBOARD="$value" ;;
   esac
